3d0b046d41873ff35811de522e3009ea627f8905
Gustavo Rigor <gusrigor@icloud.com>
2022-03-29 00:20:59 -0300

    Alterado posicionamento da tabuleiro

--- js/trilha.js
+++ js/trilha.js
@@ -1,14 +1,15 @@
 const trilha ={
+    //TODO: Criar um objeto para os espaço da trilha
     tabuleiro: [
-        '','','',
-        '','','',
-        '','','',
-        '','','', '','','',
-        '','','',
-        '','','',
-        '','','',
+        {"p":"A7"},{"p":"D7"},{"p":"G7"},
+        {"p":"B6"},{"p":"D6"},{"p":"F6"},
+        {"p":"C5"},{"p":"D5"},{"p":"E5"},
+        {"p":"A4"},{"p":"B4"},{"p":"C4"}, {"p":"E4"},{"p":"F4"},{"p":"G4"},
+        {"p":"C3"},{"p":"D3"},{"p":"E3"},
+        {"p":"B2"},{"p":"D2"},{"p":"F2"},
+        {"p":"A1"},{"p":"D1"},{"p":"G1"}
     ],
-    simbolos: ['p','b'],
+    simbolos: ['preto','branco', 'vazio'],
     container_element: null,
 
     init: function(container){
@@ -18,9 +19,10 @@ const trilha ={
     draw: function(){
         let content = '';
 
-        for (i in this.tabuleiro){
-            content += '<div> <div class="bola">' + i + '</div> </div>';
-        }
+        this.tabuleiro.forEach(function(homem){
+            content += '<div class="'+ homem.p +'"> <div class="bola">' + " " + '</div> </div>';
+        })
+
         this.container_element.innerHTML = content;
     },
 };
