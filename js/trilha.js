4e0b76099c2e1275d2c8e95e2875d9a81a1271b2
Gustavo Rigor <gusrigor@icloud.com>
2022-04-16 14:09:53 -0300

    Estruturando o trilha para ser uma classe

--- js/trilha.js
+++ js/trilha.js
@@ -1,22 +1,20 @@
-const trilha ={
-    //TODO: Criar um objeto para os espaço da trilha
-    tabuleiro: [
-        {"p":"A7"},{"p":"D7"},{"p":"G7"},
-        {"p":"B6"},{"p":"D6"},{"p":"F6"},
-        {"p":"C5"},{"p":"D5"},{"p":"E5"},
-        {"p":"A4"},{"p":"B4"},{"p":"C4"}, {"p":"E4"},{"p":"F4"},{"p":"G4"},
-        {"p":"C3"},{"p":"D3"},{"p":"E3"},
-        {"p":"B2"},{"p":"D2"},{"p":"F2"},
-        {"p":"A1"},{"p":"D1"},{"p":"G1"}
-    ],
-    simbolos: ['preto','branco', 'vazio'],
-    container_element: null,
-
-    init: function(container){
+class Trilha{
+    constructor(container){
+        this.tabuleiro = 
+        [
+            {"p":"A7"},{"p":"D7"},{"p":"G7"},
+            {"p":"B6"},{"p":"D6"},{"p":"F6"},
+            {"p":"C5"},{"p":"D5"},{"p":"E5"},
+            {"p":"A4"},{"p":"B4"},{"p":"C4"}, {"p":"E4"},{"p":"F4"},{"p":"G4"},
+            {"p":"C3"},{"p":"D3"},{"p":"E3"},
+            {"p":"B2"},{"p":"D2"},{"p":"F2"},
+            {"p":"A1"},{"p":"D1"},{"p":"G1"}
+        ];
+        this.simbolos = ['preto','branco', 'vazio'];
         this.container_element = container;
-    },
+    }
 
-    draw: function(){
+    draw(){
         let content = '';
 
         this.tabuleiro.forEach(function(homem){
@@ -24,5 +22,5 @@ const trilha ={
         })
 
         this.container_element.innerHTML = content;
-    },
-};
+    }
+}
